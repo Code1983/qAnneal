a8c63f102e329480cfeb2d4fcd1b5ed933e0ecf0
Malay M Das <malaydasat@gmail.com>
2019-10-30 12:08:32 -0700

    Added travis scripts

---
+++ src/QuantumTests_decoherence/runtests.jl
@@ -0,0 +1,9 @@
+@everywhere include("qAnnealv6.jl")
+n = 2
+psi = qAnneal.randomState(n)
+psiB = qAnneal.cannonical_state(n,psi,10)
+sig = qAnneal.decoherence(psiB, [1,1])
+T,s = qAnneal.anneal(n,2,1,0,psiB)
+if (@isdefined T) && (@isdefined a)
+  return 0
+end
